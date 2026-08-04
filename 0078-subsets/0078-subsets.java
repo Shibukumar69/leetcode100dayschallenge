59b928726102502bc79fb349f5823c1c8f12f173
shibu kumar <shibukumar3935@gmail.com>
2026-08-05 01:10:00 +0530

    Time: 1 ms (86.72%), Space: 44.3 MB (39.39%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
      List<List<Integer>> ans= new ArrayList<>();
+     List<Integer> current=new ArrayList<>();
      int index=0;
-      sub(nums,index,new ArrayList<>(),ans);
+      sub(nums,index,current,ans);
       return ans;
     }
     public void sub(int[] nums,int index, List<Integer> current, List<List<Integer>> ans){
+        // base case
         if(index>=nums.length){
             ans.add(new ArrayList<>(current));
             return ;
         }
+        // 1 khud se case banaye baki ka recursion karega
+
+        // including
          current.add(nums[index]);
          sub(nums,index+1,current,ans);
+          //excluding
          current.remove(current.size()-1);
          sub(nums,index+1,current,ans);
     }
