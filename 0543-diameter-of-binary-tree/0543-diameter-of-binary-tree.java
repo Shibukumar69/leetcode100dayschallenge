8ad652aefb65684a66304d0a21e27740cad93b03
shibu kumar <shibukumar3935@gmail.com>
2026-09-06 01:26:14 +0530

    Time: 178 ms (11.32%), Space: 48.7 MB (5.2%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,24 +1,41 @@
-class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-            int leftheight=height(root.left);
-            int rightheight=height(root.right);
-            int currentDiameter=leftheight+rightheight;
-            int leftDiameter=diameterOfBinaryTree(root.left);
-            int rightDiameter=diameterOfBinaryTree(root.right);
-            return Math.max(currentDiameter,Math.max(leftDiameter,rightDiameter));
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if(root==null){
+//             return 0;
+//         }
+//             int leftheight=height(root.left);
+//             int rightheight=height(root.right);
+//             int currentDiameter=leftheight+rightheight;
+//             int leftDiameter=diameterOfBinaryTree(root.left);
+//             int rightDiameter=diameterOfBinaryTree(root.right);
+//             return Math.max(currentDiameter,Math.max(leftDiameter,rightDiameter));
             
 
-        }
+//         }
     
-    static int height(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-            int leftheight=height(root.left);
-            int rightheight=height(root.right);
-            return 1+Math.max(leftheight,rightheight);
+//     static int height(TreeNode root){
+//         if(root==null){
+//             return 0;
+//         }
+//             int leftheight=height(root.left);
+//             int rightheight=height(root.right);
+//             return 1+Math.max(leftheight,rightheight);
+//     }
+// }
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        int curr = height(root.left) + height(root.right);
+        int left = diameterOfBinaryTree(root.left);
+        int right = diameterOfBinaryTree(root.right);
+
+        return Math.max(curr, Math.max(left, right));
+    }
+
+    int height(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
     }
 }
